10c43d723e51e3cb95c255a1c43002f868d6281f
wwalewski <w.walewski112@gmail.com>
2023-09-16 14:59:41 +0200

    Add tabs to room view

--- src/routes/rooms/[slug]/+page.svelte
+++ src/routes/rooms/[slug]/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-	import { Stepper, Step } from '@skeletonlabs/skeleton';
+	import { Stepper, Step, TabGroup, Tab } from '@skeletonlabs/skeleton';
 	import 'iconify-icon';
 	import TimeControls from './TimeControls.svelte';
 	import Puzzle from '$lib/components/puzzles/Puzzle.svelte';
@@ -18,22 +18,46 @@
 		activeStage: activeStageIndex
 	} = data.room;
 	const activeStage = activeStageIndex ? stages[activeStageIndex] : null;
+	let tabSet: number = 0;
 </script>
 
-<div class="container h-full mx-auto p-4">
-	<h1 class="text-3xl font-bold pb-12">{name}</h1>
-	<div class="flex flex-col gap-10 justify-center items-center w-full px-6">
-		<TimeControls room={data.room} />
-		<Stepper stepTerm="Stage" class="w-full">
-			{#each stages as stage}
-				<Step>
-					<h2 slot="header" class="text-xl">{stage.name}</h2>
-					<span class="italic text-sm">{stage.description}</span>
-					{#each stage.puzzles as puzzle}
-						<Puzzle {puzzle} />
-					{/each}
-				</Step>
-			{/each}
-		</Stepper>
-	</div>
+<div class="container h-full mx-auto px-8 py-6">
+	<h1 class="text-4xl font-bold pb-4 text-center">{name}</h1>
+	<TabGroup justify="justify-center">
+		<Tab bind:group={tabSet} name="game" value={0}>
+			<span>Game Control</span>
+		</Tab>
+		<Tab bind:group={tabSet} name="statistics" value={1}>
+			<span>Statistics</span>
+		</Tab>
+		<Tab bind:group={tabSet} name="log" value={2}>
+			<span>Logs</span>
+		</Tab>
+		<Tab bind:group={tabSet} name="config" value={3}>
+			<span>Configuration</span>
+		</Tab>
+		<!-- Tab Panels --->
+		<svelte:fragment slot="panel">
+			{#if tabSet === 0}
+				<div class="flex pt-6 flex-col gap-10 justify-center items-center w-full">
+					<TimeControls room={data.room} />
+					<Stepper stepTerm="Stage" class="w-full">
+						{#each stages as stage}
+							<Step>
+								<h2 slot="header" class="text-xl">{stage.name}</h2>
+								<span class="italic text-sm">{stage.description}</span>
+								{#each stage.puzzles as puzzle}
+									<Puzzle {puzzle} />
+								{/each}
+							</Step>
+						{/each}
+					</Stepper>
+				</div>
+			{:else if tabSet === 1}
+				(tab panel 2 contents)
+			{:else if tabSet === 2}
+				(tab panel 3 contents)
+			{/if}
+		</svelte:fragment>
+	</TabGroup>
 </div>
