39d0f4e79e547740d5f2fa2a8ff3fabfc3445cca
wwalewski <w.walewski112@gmail.com>
2023-09-16 15:03:37 +0200

    Fix room card width in dashboard.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,9 +6,9 @@
 </script>
 
 <div class="container h-full mx-auto flex justify-center items-center p-12">
-	<ul class="flex flex-wrap flex-row justify-center gap-4">
+	<ul class="flex w-full flex-wrap flex-row justify-center gap-4">
 		{#each data.rooms as room}
-			<li class="w-96 xl:w-[49%]">
+			<li class="w-96 xl:w-[49%] max-w-lg">
 				<RoomOverviewCard {room} />
 			</li>
 		{/each}
